6dfd74cced833768e1425e06390e6bdc32dd0019
Pedro <pedrohenriquefa97@gmail.com>
2025-05-26 22:38:38 -0300

    Refactor add_produto method to improve product quantity handling and error messaging

--- main.py
+++ main.py
@@ -7,14 +7,27 @@ class Estoque:
         
     def add_produto(self, id, nome, quantidade):
         if id in self.produtos:
-            self.produtos[id][nome] += quantidade
+            if self.produtos[id]['nome'] != nome:
+                print(f'Erro: ID {id} já existe para o produto {self.produtos[id]["nome"]}.')
+                return None
+            self.produtos[id]['quantidade'] += quantidade
         else:
-            self.produtos[id] = {nome:quantidade}
+            self.produtos[id] = {'nome':nome, 'quantidade': quantidade}
         self.espaco -= quantidade
+        
+        print(f'A capcidade livre do estoque é de {self.espaco} itens atualmente.\n')
+    
+    def buscar_produto(self, id):
+        if id in self.produtos:
+            produto = self.produtos[id]
+            print(f'Existem {produto["quantidade"]} unidades de {produto["nome"]} no estoque {self.nome}')
 
+    def mostra_estoque(self):
+        pass
+    
 e1 = Estoque('Amazon', 1000)
 e2 = Estoque('Americanas', 800)
 
 e1.add_produto('20', 'Caneta', 30)
-print(e1.produtos)
-print(e1.espaco)
+e1.add_produto('30', 'Lapis', 30)
+e1.buscar_produto('20')
